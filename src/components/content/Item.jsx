7a82daff4cfaff40c4eb09e5ecf5fd6d2c2c2446
srarig <starkovivan3@gmail.com>
2022-06-24 12:52:25 +0800

    Фильтрация работает при клике на item

--- src/components/content/Item.jsx
+++ src/components/content/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setActiveCategory, setType} from "../../redux/slices/filterSlice";
 
 const Item = ({
@@ -9,10 +9,26 @@ const Item = ({
     id
               }) => {
     const dispatch = useDispatch();
+    const { activeCategory, sortList, activeType } = useSelector((state) => state.filter);
+    console.log(sortList)
+
+    const types = [];
+
+    sortList.forEach(item => types.push(item.sortType));
+
+    console.log(types);
+
+    const setType = (type) => {
+        const currentTypeIndex = types.indexOf(type);
+        dispatch(setActiveCategory(currentTypeIndex));
+    }
+
     return (
         <div className="item">
             <img src={imgUrl} alt="item image" className="itemImg"/>
-            <div className="itemType">{type}</div>
+            <div className="itemType" onClick={() => {
+                setType(type);
+            }}>{type}</div>
             <div className="itemTitle">{title}</div>
         </div>
     )
